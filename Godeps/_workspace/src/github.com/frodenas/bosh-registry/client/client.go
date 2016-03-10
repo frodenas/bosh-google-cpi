a372e93e97ea03c4beb7965895b78f9020236c3a
Evan Brown <evanbrown@google.com>
2016-03-10 14:37:20 -0800

    Use Go1.6 vendoring

--- Godeps/_workspace/src/github.com/frodenas/bosh-registry/client/client.go
+++
@@ -1,8 +0,0 @@
-package registry
-
-// Client represents a BOSH Registry Client.
-type Client interface {
-	Delete(instanceID string) error
-	Fetch(instanceID string) (AgentSettings, error)
-	Update(instanceID string, agentSettings AgentSettings) error
-}
